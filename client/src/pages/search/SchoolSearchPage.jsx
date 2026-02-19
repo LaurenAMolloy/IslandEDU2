3ef74d6aa3321d5ad67fa09d25618a05164090bd
Paul Hammond <paul.hammond@gmail.com>
2026-02-19 12:33:10 +0000

    Fixing first test issue

--- client/src/pages/search/SchoolSearchPage.jsx
+++ client/src/pages/search/SchoolSearchPage.jsx
@@ -1,15 +1,14 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData } from "react-router-dom";
 
 export default function SchoolSearchPage() {
   // This hook gets the data directly from the loader in App.jsx
-  const { featuredSchools} = useLoaderData();
-  console.log(featuredSchools)
+  const { featuredSchools } = useLoaderData();
+  console.log(featuredSchools);
 
   return (
     <div className="search-container">
       <h2>Available Schools in Cyprus</h2>
-      <div className="school-list">
-      </div>
+      <div className="school-list"></div>
     </div>
   );
 }
--- client/src/pages/search/SchoolSearchPage.test.jsx
+++ client/src/pages/search/SchoolSearchPage.test.jsx
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router';
-import SchoolSearchPage from './SchoolSearchPage';
-import { test, vi, expect, within} from 'vitest'
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import { expect, test, vi, within } from "vitest";
+import SchoolSearchPage from "./SchoolSearchPage";
 
 //What are we testing for?
 //Does Loader provide the data we require?
@@ -9,39 +9,37 @@ import { test, vi, expect, within} from 'vitest'
 //Are there 4 cards?
 //Do they have content?
 
+vi.mock("react-router-dom", async () => {
+  const actual = await vi.importActual("react-router-dom");
+  return {
+    ...actual,
+    useLoaderData: () => ({
+      featuredSchools: [
+        { _id: "1", name: "School One" },
+        { _id: "2", name: "School Two" },
+        { _id: "3", name: "School Three" },
+        { _id: "4", name: "School Four" },
+      ],
+    }),
+  };
+});
 
-vi.mock('react-router', async () => {
-    const actual = await vi.importActual('react-router')
-    return {
-        ...actual,
-        useLoaderData: () => ({
-                schools: [
-                { _id: "1", name: "School One" },
-                { _id: "2", name: "School Two" },
-                { _id: "3", name: "School Three" },
-                { _id: "4", name: "School Four" },
-            ]
-        })
-    }
-})
+test("loader provides four schools", () => {
+  render(
+    <MemoryRouter>
+      <SchoolSearchPage />
+    </MemoryRouter>,
+  );
 
-test('loader provides four schools', () => {
+  expect(screen.getByText("School One")).toBeInTheDocument();
+  expect(screen.getByText("School Two")).toBeInTheDocument();
+  expect(screen.getByText("School Three")).toBeInTheDocument();
+  expect(screen.getByText("School Four")).toBeInTheDocument();
 
-    render(
-        <MemoryRouter>
-            <SchoolSearchPage />
-        </MemoryRouter>
-    )
+  const cards = screen.getAllByRole("article");
+  expect(cards).toHaveLength(4);
 
-    expect(screen.getByText("School One")).toBeInTheDocument();
-    expect(screen.getByText("School Two")).toBeInTheDocument();
-    expect(screen.getByText("School Three")).toBeInTheDocument();
-    expect(screen.getByText("School Four")).toBeInTheDocument();
-
-    const cards = screen.getAllByRole('article');
-    expect(cards).toHaveLength(4);
-
-    cards.forEach(card => {
-        expect(within(card).getByRole('heading')).toBeInTheDocument();
-    })
-})
+  cards.forEach((card) => {
+    expect(within(card).getByRole("heading")).toBeInTheDocument();
+  });
+});
